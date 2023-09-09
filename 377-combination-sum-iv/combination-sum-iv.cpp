54e7964a2a96492d644947d78bebeb844f4b451a
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2023-09-09 13:58:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (6.68%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+     int n;
+     int t[201][1001];
+     int solve(int idx,vector<int>& nums,int target){
+         if(target==0) return 1;
+         
+         if(idx>=n ||target<0) return 0;
+           if(t[idx][target]!=-1) return t[idx][target];
+         int take =solve(0,nums,target-nums[idx]);
+         int skip =solve(idx+1,nums,target);
+
+         return  t[idx][target]=take+skip;
+     }
+    int combinationSum4(vector<int>& nums, int target) {
+      n=nums.size();
+      memset(t,-1,sizeof(t));
+      return solve(0,nums,target);  
+    }
+};
