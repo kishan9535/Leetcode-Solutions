a6d3bf852840493bcf059d84e7af8cd7626cf485
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2023-10-26 22:21:57 +0530

    Time: 68 ms (77.63%) | Memory: 56.8 MB (60.10%) - LeetSync

---
+++ 2437-maximum-number-of-groups-entering-a-competition/maximum-number-of-groups-entering-a-competition.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maximumGroups(vector<int>& grades) {
+       int x=1;
+       int n=grades.size();
+       while(x*(x+1)/2 <=n)x++;
+
+       return --x;
+    }
+};
