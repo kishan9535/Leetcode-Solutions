74b1df5838ece8e9aa53ccf445a837cf2c603ff3
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2023-12-25 21:30:03 +0530

    Time: 58 ms (19.53%) | Memory: 7.1 MB (44.44%) - LeetSync

--- 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.cpp
+++ 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.cpp
@@ -8,7 +8,8 @@ public:
 
         int has1=0;
         for(int i=0;i<n;i++){
-         if(i+1<n &&(s[i]=='0' &&s[i+1]=='1')){
+         if(i+1<n &&(s[i]=='0' &&s
+         [i+1]=='1')){
              swap(pres[i],pres[i+1]);
              has1=1;
          }
