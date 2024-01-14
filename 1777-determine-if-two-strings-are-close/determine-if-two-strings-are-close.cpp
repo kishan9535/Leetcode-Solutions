cb5295f707419294db41174d77ea5f5629c9a5dd
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2024-01-14 18:43:58 +0530

    Time: 61 ms (95.82%) | Memory: 21 MB (60.02%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-      vector<int> count1(26, 0), count2(26, 0);
-    for (char c : word1) count1[c - 'a']++;
-    for (char c : word2) count2[c - 'a']++;
-    for (int i = 0; i < 26; ++i) {
-        if ((count1[i] && !count2[i]) || (!count1[i] && count2[i])) return false;
-    }
-    sort(count1.begin(), count1.end());
-    sort(count2.begin(), count2.end());
-    return count1 == count2;  
+      vector<int>c1(26,0);
+      vector<int>c2(26,0);
+      int n=word1.size();
+      int m=word2.size();
+      if(n!=m)return false;
+      for(auto i:word1){
+          c1[i-'a']++;
+      }  
+      for(auto i:word2){
+          c2[i-'a']++;
+      }
+      for(int i=0;i<26;i++){
+          if(c1[i]==0 &&c2[i]!=0) return false;
+          if(c1[i]!=0 && c2[i]==0) return false;
+          
+
+      }
+      sort(c1.begin(),c1.end());
+          sort(c2.begin(),c2.end());
+          return c1==c2;  
     }
 };
