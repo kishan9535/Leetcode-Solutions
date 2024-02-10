cae15feaa540943fa490e02f5d84d55141a5663d
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2024-02-10 11:33:03 +0530

    Time: 387 ms (11.66%) | Memory: 11.9 MB (53.19%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
+int t[1001][1001];
     bool isPalindrome(string &s,int i,int j){
         if(i>j)return true;
+        if(t[i][j]!=-1)return t[i][j];
         if(s[i]==s[j]){
             return isPalindrome(s,i+1,j-1);
         }
@@ -9,6 +11,7 @@ public:
     }
     int countSubstrings(string s) {
      int ans=0;
+     memset(t,-1,sizeof(t));
         int n=s.length();
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
