48779eb6ed93c7d353109290f92eeaae71735969
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2024-03-06 22:46:45 +0530

    Time: 160 ms (45.80%) | Memory: 103.4 MB (21.55%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+   int binary_search(int key,vector<int>& potions){
+       int l=0;
+       int r=potions.size()-1;
+       int ans=-1;
+       while(l<=r){
+           int mid=l+(r-l)/2;
+           if(potions[mid]>=key){
+               ans=mid;
+               r=mid-1;
+           }
+           else{
+               l=mid+1;
+           }
+       }
+       return ans;
+   }
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
       int n=spells.size();
       int m=potions.size();
@@ -15,7 +31,9 @@ public:
             ans.push_back(0);
             continue;
         }
-        int idx=lower_bound(potions.begin(),potions.end(),min_pos)-potions.begin();
+        //int idx=lower_bound(potions.begin(),potions.end(),min_pos)-potions.begin();
+         int idx=binary_search(min_pos,potions);
+
         int cnt=m-idx;
         ans.push_back(cnt);
       }  
