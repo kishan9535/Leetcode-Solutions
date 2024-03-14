e5a1120976e120323cb45e6cff60fdf36f57ca63
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2024-03-14 22:19:32 +0530

    Time: 52 ms (6.26%) | Memory: 25.1 MB (7.56%) - LeetSync

--- 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.cpp
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int longestContinuousSubstring(string s) {
-       int n = s.length();
-        int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            int cnt = 1; // Initialize count for the current substring
-            while (i < n - 1 && s[i] + 1 == s[i + 1]) {
-                cnt++; // Extend the current substring
-                i++;
-            }
-            ans = max(ans, cnt); // Update the maximum length
+     int n=s.length();
+     vector<int>nums(n,0);
+     for(int i=0;i<n;i++){
+        nums[i]=s[i]-'0';
+     }
+     int ans=0;
+     int i=0;
+       while (i < n) {
+        int cnt = 1; // Initialize count for the current substring
+        while (i < n - 1 && nums[i]+1 == nums[i + 1]) {
+            cnt++; // Extend the current substring
+            i++;
         }
-
-        return ans;  
+        ans = max(ans,cnt); // Update the maximum length
+        i++;
+    }
+     return ans;    
     }
 };
