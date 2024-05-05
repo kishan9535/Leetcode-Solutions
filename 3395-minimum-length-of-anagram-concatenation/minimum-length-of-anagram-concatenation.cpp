c0619a8849363c1033e17a91ceed8ebbd6b7f8c3
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2024-05-05 21:40:56 +0530

    Time: 212 ms (16.67%) | Memory: 46.5 MB (33.33%) - LeetSync

--- 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.cpp
+++ 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     bool Check(string s ,int k){
+        int d;
+       /* unordered_map<int,int>mp;
+        for(int i=0;i<k;k++){
+            mp[s[i]]++;
+        }*/
         string st=s.substr(0,k);
         sort(st.begin(),st.end());
+        
         for(int i=k;i<s.length();i+=k){
             string temp=s.substr(i,k);
             sort(temp.begin(),temp.end());
