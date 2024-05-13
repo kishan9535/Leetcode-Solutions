88a7a25cd5c7418c05f876aad0063d8e6848d846
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2024-05-13 23:13:29 +0530

    Time: 153 ms (50.00%) | Memory: 148.9 MB (100.00%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int n;
-    vector<int> dp;
-
-    int solve(int idx, vector<int>& energy, int k) {
-        if (idx >= n) return 0;
-        if (dp[idx] != -1) return dp[idx]; // Return stored result if available
-        int pick = energy[idx] + solve(idx + k, energy, k);
-        return dp[idx] = pick; // Store result before returning
+int n;
+    int t[100001];
+    int solve(int idx,vector<int>& energy, int k){
+        if(idx>=n)return 0;
+        if(t[idx]!=-1)return t[idx];
+        int pick=energy[idx]+solve(idx+k,energy,k);
+        
+        return t[idx]=pick;
     }
-
     int maximumEnergy(vector<int>& energy, int k) {
-        n = energy.size();
-        dp = vector<int>(n, -1); // Initialize dp vector with -1
-        int maxi = INT_MIN;
+      n=energy.size();
+      memset(t,-1,sizeof(t));
+      int maxi = INT_MIN;
         for (int i = 0; i < n; i++) { // Loop through all starting points
             maxi = max(maxi, solve(i, energy, k));
         }
-        return maxi;
+        return maxi; 
     }
 };
