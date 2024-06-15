104102c1320d283cee14d1281d65295d71f6a40b
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2024-06-15 21:46:30 +0530

    Time: 149 ms (44.75%) | Memory: 81.7 MB (83.55%) - LeetSync

---
+++ 502-ipo/ipo.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+      
+      int n=profits.size();
+      vector<pair<int,int>>ans(n);
+      for(int i=0;i<n;i++){
+        ans[i]={capital[i],profits[i]};
+      }
+      sort(ans.begin(),ans.end());
+
+      priority_queue<int>pq;
+      int i=0;
+
+      while(k--){
+
+        while(i<n && ans[i].first<=w){
+            pq.push(ans[i].second);
+            i++;
+        }
+          if(pq.empty()){
+            return w;
+          }
+        w+=pq.top();
+        pq.pop();
+      }
+      return w;  
+    }
+};
