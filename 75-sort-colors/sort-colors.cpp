4006d46a9d150e0a4789d7bbd00bca7da6ef4971
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2024-12-01 21:03:57 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (5.09%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-    //   vector<int>cnt(3,0);
-    //   for(auto it: num){
+      vector<int>cnt(3,0);
+    //   for(int i=0;i<nums.size();i++){
     //      if(nums[i]==0)cnt[0]++;
     //      else if(nums[i]==2)cnt[1]++;
     //      else cnt[2]++;
     //   }
-    //   int n= nums.size();
-    //   int i=0;
-    //   int j=0;
-    //   while(i<n && j<cnt[0]){
-    //     nums[i]=0;
-    //   }
-    //   j=0
-    //   while(i<n && j<cnt[1]){
-    //     nums[i]=1;
-    //   }
-    //   j=0;
-    //   while(i<n && j<cnt[2]){
-    //     nums[i]=2;
-    //   }   
-    sort(nums.begin(),nums.end()); 
+      for(auto it: nums){
+        if(it==0)cnt[0]++;
+        if(it==1)cnt[1]++;
+        if(it==2)cnt[2]++;
+      }
+      cout<<cnt[0]<<" "<<cnt[1]<<" "<<cnt[2];
+      int n= nums.size();
+      int i=0;
+      int k=cnt[0];
+      while( k--){
+        nums[i]=0;
+        i++;
+      }
+      k=cnt[1];
+      while(k--){
+        nums[i]=1;
+        i++;
+      }
+      k=cnt[2];
+      while(k--){
+        nums[i]=2;
+        i++;
+      }    
     }
 };
