48980bc8a1d16ad9bf8a4d0578e4919767b6fc4b
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2025-01-04 20:06:13 +0530

    Time: 167 ms (66.57%) | Memory: 15.9 MB (56.14%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-      int n=s.length();
-      int result=0;
-      vector<pair<int,int>>indi(26,{-1,-1});
-      for(int i=0;i<n;i++){
-         char ch=s[i];
-         int idx=ch-'a';
-          if(indi[idx].first==-1){
-              indi[idx].first=i;
-          }
-          indi[idx].second=i;
-      }
-      for(int i=0;i<26;i++){
-          if(indi[i]==make_pair(-1,-1)){
-              continue;
-          }
-          unordered_set<char>st;
-          for(int count =indi[i].first+1;count<=indi[i].second-1;count++){
-              st.insert(s[count]);
-          }
-          result+=st.size();
-      }  
-      return result;
+       int ans=0;
+       unordered_map<char,pair<int,int>>mp;
+       int n=s.length();
+       for(char ch='a';ch<='z';ch++){
+         int first=-1,last=-1;
+         for (int i = 0; i < n; i++) {
+                if (s[i] == ch) {
+                    if (first == -1) {
+                        first = i; 
+                    }
+                    last = i;  
+                }
+            }
+            mp[ch] = {first, last};
+       }
+       for(auto it:mp){
+        char c=it.first;
+         int f = it.second.first;
+         int l = it.second.second;
+        if(f==-1||l==-1 || f==l)continue;
+        unordered_set<char>st;
+        for(int i=f+1;i<l;i++){
+            st.insert(s[i]);
+        }
+        ans+=st.size();
+       }
+       return ans;
     }
 };
