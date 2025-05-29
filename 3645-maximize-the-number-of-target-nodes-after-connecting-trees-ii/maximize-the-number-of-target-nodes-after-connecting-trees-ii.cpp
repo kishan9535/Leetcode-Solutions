a69a13be607382c2bf72e060117b6724aaa1482a
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2025-05-29 17:43:46 +0530

    Time: 463 ms (25.17%) | Memory: 397.6 MB (10.88%) - LeetSync

---
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -0,0 +1,54 @@
+class Solution {
+   
+
+    //Optimize solution in O(N) TC
+    void dfs(int curr, unordered_map<int,vector<int>>&adj, int parent, int &evenCnt, int &oddCnt, vector<int>& parity,int depth){
+            if(depth & 1){
+                oddCnt++;
+                parity[curr] = 1;
+            }else{
+                evenCnt++;
+                parity[curr] = 0;
+            }
+
+            for(auto& n : adj[curr]){
+                if(n != parent)
+                 dfs(n, adj, curr, evenCnt, oddCnt, parity, depth + 1);
+            }
+    }
+
+
+    vector<int>findCnt(vector<vector<int>>& edges){
+        int n = edges.size() + 1;
+        unordered_map<int,vector<int>>adj;
+        for(auto & edge : edges){
+           int u = edge[0];
+           int v = edge[1];
+           adj[u].push_back(v);
+           adj[v].push_back(u);
+        }
+
+        int evenCnt = 0, oddCnt = 0;
+        vector<int>parity(n, 0);
+        dfs(0, adj, -1, evenCnt, oddCnt, parity, 0);
+
+        vector<int>res(n);
+        for(int i = 0; i<n; i++){
+           
+            res[i] = (parity[i] == 0 ? evenCnt : oddCnt);  
+        }
+        return res;
+    }
+public:
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+        int N = edges1.size() + 1;
+        vector<int>res1 = findCnt(edges1);
+        vector<int>res2 = findCnt(edges2);
+        int maxCnt = *max_element(res2.begin(),res2.end());
+
+        for(int i = 0; i<res1.size(); i++){
+            res1[i] += maxCnt;
+        }
+        return res1;
+    }
+};
