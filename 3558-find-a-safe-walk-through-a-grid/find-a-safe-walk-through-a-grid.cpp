9c84571cf96781dc61563ede46457c289ce5e167
Kishan Kumar N <103923290+kishan9535@users.noreply.github.com>
2026-07-02 20:17:11 +0530

    Time: 109 ms (18.80%) | Memory: 52.8 MB (17.57%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
-    int dir_r[4] = {1,-1,0,0};
-    int dir_c[4] = {0,0,1,-1};
-    int dp[51][51][102];
-    bool recurs(vector<vector<int>>& grid, vector<vector<int>>& visited,int health,int i,int j){
-        int n = grid.size(),m=grid[0].size();
-        if(i==n-1&&j==m-1&&health-grid[i][j]>0)
-            return 1;
-        if(i>=n || j>=m || health<=0 || i<0 || j<0)
-            return 0;
+    bool findSafeWalk(vector<vector<int>>& grid, int health){
 
-        if(dp[i][j][health] != -1)
-            return dp[i][j][health];
+        int m=grid.size();
+        int n=grid[0].size();
+        deque<pair<int,int>>dq;
+        vector<vector<int>>ans(m,vector<int>(n,INT_MAX));
+        ans[0][0]=grid[0][0];
+        dq.push_front({0,0});
 
-        bool ans = false;
+        vector<vector<int>>dir{{0,1},{1,0},{0,-1},{-1,0}};
 
-        for(int idx = 0;idx<4;idx++){
-            int x = i+dir_r[idx],y = j+dir_c[idx];
-            if(x<0||y<0||x>=n||y>=m||visited[x][y])
-                continue;
-            visited[x][y] = 1;
-            ans |= recurs(grid,visited,health-grid[i][j],x,y);
-            if(ans)
-                return dp[i][j][health]=ans;
-            visited[x][y] = 0;
+        while(!dq.empty()){
+            auto it=dq.front();
+            dq.pop_front();
+            int i=it.first;
+            int j=it.second;
+
+            for(auto d:dir){
+                int ni=d[0]+i;
+                int nj=d[1]+j;
+
+                if(ni<0 ||ni>=m ||nj<0||nj>=n)continue;
+
+                if(ans[ni][nj]>grid[ni][nj]+ans[i][j]){
+                    ans[ni][nj]=grid[ni][nj]+ans[i][j];
+
+                    if(grid[ni][nj]==0){
+                        dq.push_front({ni,nj});
+                    }
+                    else{
+                        dq.push_back({ni,nj});
+                    }
+                }
+            }
+
+          
         }
-        return dp[i][j][health]=ans;
-    }
-    bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        int n = grid.size(),m=grid[0].size();
-        memset(dp,-1,sizeof(dp));
-        vector<vector<int>>visited(n,vector<int>(m,0));
-        
-        return recurs(grid,visited,health,0,0);
+
+        return (health-(ans[m-1][n-1]))>=1;
+
     }
 };
